37af52eb0e85f43e82d68775332e18ce30bed277
Anthony Beaver <anthonybeavermusic@gmail.com>
2020-08-08 19:30:14 -0700

    lab07 completed

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 Got the Tokyo object completed and rendered.
 8-7 Updated the JS and HTML for Dubai.
 Updated Paris and Lima.
+
+8-8: Completed all the logic for the Js code. Made some wicked cool styles for the css and called it a night.
--- js/app.js
+++ js/app.js
@@ -2,158 +2,112 @@
 
 var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var seattle = {
-  location: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avCookSale: 6.3,
-  hourlySalesArray: [],
-  dailySalesTotal: 0,
-  calcHourlySalesArray: function () {
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
-      this.hourlySalesArray.push(hourlySales);
-      this.dailySalesTotal += hourlySales;
-    }
-  },
-  render: function() {
-    this.calcHourlySalesArray();
-    //remember to change the element ID.
-    var ulEl = document.getElementById('seattle');
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOperation[i]}: ${this.hourlySalesArray[i]} cookies`;
-      ulEl.append(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
-    ulEl.append(liEl);
-  }
-};
+var totalsForEachHour = [];
+var totalOfAllLocationsAllDay = 0;
 
-var tokyo = {
-  location: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avCookSale: 1.2,
-  hourlySalesArray: [],
-  dailySalesTotal: 0,
-  calcHourlySalesArray: function () {
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
-      this.hourlySalesArray.push(hourlySales);
-      this.dailySalesTotal += hourlySales;
-    }
-  },
-  render: function() {
-    this.calcHourlySalesArray();
-    //remember to change the getElementById.
-    var ulEl = document.getElementById('tokyo');
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOperation[i]}: ${this.hourlySalesArray[i]} cookies`;
-      ulEl.append(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
-    ulEl.append(liEl);
-  }
-};
-
-var dubai = {
-  location: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  avCookSale: 3.7,
-  hourlySalesArray: [],
-  dailySalesTotal: 0,
-  calcHourlySalesArray: function () {
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
-      this.hourlySalesArray.push(hourlySales);
-      this.dailySalesTotal += hourlySales;
-    }
-  },
-  render: function() {
-    this.calcHourlySalesArray();
-    //remember to change the getElementById.
-    var ulEl = document.getElementById('dubai');
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOperation[i]}: ${this.hourlySalesArray[i]} cookies`;
-      ulEl.append(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
-    ulEl.append(liEl);
-  }
-};
-
-var paris = {
-  location: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  avCookSale: 2.3,
-  hourlySalesArray: [],
-  dailySalesTotal: 0,
-  calcHourlySalesArray: function () {
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
-      this.hourlySalesArray.push(hourlySales);
-      this.dailySalesTotal += hourlySales;
-    }
-  },
-  render: function() {
-    this.calcHourlySalesArray();
-    //remember to change the getElementById.
-    var ulEl = document.getElementById('paris');
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOperation[i]}: ${this.hourlySalesArray[i]} cookies`;
-      ulEl.append(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
-    ulEl.append(liEl);
-  }
-};
-
-var lima = {
-  location: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  avCookSale: 4.6,
-  hourlySalesArray: [],
-  dailySalesTotal: 0,
-  calcHourlySalesArray: function () {
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
-      this.hourlySalesArray.push(hourlySales);
-      this.dailySalesTotal += hourlySales;
-    }
-  },
-  render: function() {
-    this.calcHourlySalesArray();
-    //remember to change the getElementById.
-    var ulEl = document.getElementById('lima');
-    for (var i = 0; i < hoursOfOperation.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursOfOperation[i]}: ${this.hourlySalesArray[i]} cookies`;
-      ulEl.append(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
-    ulEl.append(liEl);
-  }
-};
-
-// source from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// Creating the constructor for all objects.
+function Store(location, minCust, maxCust, avCookieSale) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avCookSale = avCookieSale;
+  this.hourlySalesArray = [];
+  this.dailySalesTotal = 0;
 }
 
+Store.prototype.calcHourlySalesArray = function () {
+  for (var i = 0; i < hoursOfOperation.length; i++) {
+    var hourlySales = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.avCookSale);
+    this.hourlySalesArray.push(hourlySales);
+    this.dailySalesTotal += hourlySales;
+  }
+};
+
+// Creating hours of operation table elements
+var hoursTableEl = document.getElementById('table-head');
+var hoursRowsEl = document.createElement('tr');
+var hoursTitleDataEl = document.createElement('th');
+hoursTitleDataEl.textContent = 'Hours';
+hoursRowsEl.append(hoursTitleDataEl);
+hoursTableEl.append(hoursRowsEl);
+for (var i = 0; i < hoursOfOperation.length; i++) {
+  var hoursDataEl = document.createElement('th');
+  hoursDataEl.textContent = hoursOfOperation[i];
+  hoursRowsEl.append(hoursDataEl);
+  hoursTableEl.append(hoursRowsEl);
+}
+var hoursLastEl = document.createElement('th');
+hoursLastEl.textContent = 'Cookies sold';
+hoursRowsEl.append(hoursLastEl);
+
+//Redering the locations with a prototype function
+Store.prototype.render = function () {
+  this.calcHourlySalesArray(); //this is calling the fuction to create the data for the hourlySalesArray.
+  var tableEl = document.getElementById('table-body'); // element ID for the table goes here
+  var trEl = document.createElement('tr');
+  var locationHeadEl = document.createElement('th');
+  locationHeadEl.textContent = this.location;
+  trEl.append(locationHeadEl);
+  tableEl.append(trEl);
+  for (var i = 0; i < hoursOfOperation.length; i++) {
+    var salesDataEl = document.createElement('td');
+    salesDataEl.textContent = this.hourlySalesArray[i];
+    trEl.append(salesDataEl);
+  }
+  salesDataEl = document.createElement('td'); // total sales data
+  //console.log(this.dailySalesTotal);
+  salesDataEl.textContent = `Total: ${this.dailySalesTotal} cookies`;
+  trEl.append(salesDataEl);
+  tableEl.append(trEl);
+};
+
+// All locations as new instances
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 2.3);
+var lima = new Store('Lima', 2, 16, 4.6);
+// storing all locations in an array
+var allLocations = [seattle, tokyo, dubai, paris, lima];
+
+//Rendering
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+
+// we are calculating the total of all sales of each hour with totalOfAllLocationsAllDay.
+// we are calculating the total of each hour and each location with hourTotal.
+function getTotalHourSales() {
+  for(i = 0; i < hoursOfOperation.length; i++) {
+    var hourTotal = 0;
+    for(var j = 0; j < allLocations.length; j++) {
+      hourTotal += allLocations[j].hourlySalesArray[i];
+    }
+    totalOfAllLocationsAllDay += hourTotal;
+    totalsForEachHour[i] = hourTotal;
+  }
+}
+getTotalHourSales();
+
+// table foot
+var totalsOfAllEl = document.getElementById('table-foot');
+var totalsRowsEl = document.createElement('tr');
+var totalsDataEl = document.createElement('th');
+totalsDataEl.textContent = 'Totals';
+totalsRowsEl.append(totalsDataEl);
+totalsOfAllEl.append(totalsRowsEl);
+for (i = 0; i < hoursOfOperation.length; i++) {
+  var totalHourlyData = document.createElement('td');
+  totalHourlyData.textContent = totalsForEachHour[i];
+  totalsRowsEl.append(totalHourlyData);
+}
+var grandTotalEl = document.createElement('th');
+grandTotalEl.textContent = `Grand total: ${totalOfAllLocationsAllDay} cookies.`;
+totalsRowsEl.append(grandTotalEl);
+
+// source from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
